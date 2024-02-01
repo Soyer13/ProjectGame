7f9057822917ec23ad02db40f05b309ff708b2ec
SoyerBC <157618382+SoyerBC@users.noreply.github.com>
2024-02-01 11:54:13 +0100

    EndGameSign

---
+++ Game/Assets/Scripts/EndLevelSign.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndLevelSign : MonoBehaviour
+{
+    private BoxCollider myColider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myColider = GetComponent<BoxCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            GameManager.instance.LoadLevel(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
