7d9307071977519c397beaafd3f6bd2e1060739d
SoyerBC <157618382+SoyerBC@users.noreply.github.com>
2024-02-01 13:43:00 +0100

    FirstAid Sign

---
+++ Game/Assets/Scripts/FirstAid.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstAid : MonoBehaviour
+{
+    // Start is called before the first frame update
+    BoxCollider myboxCider;
+    void Start()
+    {
+        myboxCider = GetComponent<BoxCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            GameManager.instance.PlayerLife += 1;
+            Destroy(this.gameObject);
+        }
+    }
+}
